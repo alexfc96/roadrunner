f21a82e9a092ed9ef5521e56c9603329712610bd
alex <alex@PC-AZUL.localdomain>
2020-01-26 13:06:55 +0100

    the buttons are working and change the screen of the game(start, gamescreen)

--- code/js/main.js
+++ code/js/main.js
@@ -1,21 +1,18 @@
-class Main {
-    constructor(){
-        this.gameSpace = document.getElementById("space-game")
-    }
+document.addEventListener('DOMContentLoaded', (event) => {
 
-    createStartScreen(){
-        //console.log("createStartScreen")
-        const startScreen = document.getElementById("start-screen");
-        startScreen.innerHTML = `
+    function createStartScreen(){ //the first display showing the title of the game
+        console.log("createStartScreen")
+        const startScreen = document.getElementById("start-screen"); //selecting the div that will contain the structure of the display
+        startScreen.innerHTML = `                       
         <h2> Welcome to RoadRunneR</h2> 
         <input id="btn-start-game" type="button" value="START GAME"> 
-        <p>Game created by: Alex Fernández</p>`
+        <p>Game created by: Alex Fernández</p>`  //adding the code
 
-        //return startScreen
     }
-    createGameScreen(){
-        //console.log("createGameScreen")
-        const gameScreen = document.getElementById("game-screen");
+    function createGameScreen(){
+        console.log("createGameScreen")
+        //startScreen.innerHTML = ``
+        startScreen.style = "display: none";   //hiding the startScreen for put the gameScreen
         gameScreen.innerHTML = `            
         <div class="rows-blocks">
             <div class="block"></div>
@@ -43,16 +40,26 @@ class Main {
             <div class="block"></div>
         </div>`
     }
-    createGameOverScreen(){
-        //console.log("createGameOverScreen")
-        const gameOverScreen = document.getElementById("game-over-screen");
+    function createGameOverScreen(){
+        console.log("createGameOverScreen");
+        gameScreen.style = "display: none"; //hidding the gameScreen
         gameOverScreen.innerHTML = `             
         <p>Bien jugado.</p>
         Play again?<br>
         <button id="btn-replay-game" value="RESTART GAME">RESTART GAME</button>`
     }
+    
+    const startScreen = document.getElementById("start-screen");
+    const btnStartGameScreen = document.getElementById("btn-start-game");
+    btnStartGameScreen.addEventListener('click',createGameScreen);
+    const gameScreen = document.getElementById("game-screen");
+    const gameOverScreen = document.getElementById("game-over-screen");
 
-}
+})
+
+//main = new Main()
+
+
 
 //createStartScreen(spaceGame);
 //createGameScreen(id);
